4360a2543506ccdddff5d8b4882bcc61092f3c04
Clavianus Juneardo <clavianus.juneardo@traveloka.com>
2022-02-12 14:32:20 +0700

    add initial bootstrap (#1)
    Signed-off-by: Clavianus Juneardo <clavianus.juneardo@traveloka.com>

---
+++ tools.mk
@@ -0,0 +1,18 @@
+# Copyright 2022 ClavinJune/bjora
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+licenser := github.com/liamawhite/licenser@v0.6.0
+linter   := github.com/golangci/golangci-lint/cmd/golangci-lint@v1.44.0
+releaser := github.com/goreleaser/goreleaser@v1.4.1
+wire     := github.com/google/wire/cmd/wire@v0.5.0
